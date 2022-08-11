cebe4b2688d4c795f18d98ded4f9b90453afc17b
v_bbinxiong <you@example.com>
2022-08-11 10:30:24 +0800

    fix:路由重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/',
+    redirect: '/home/about'
+  },
+  {
     path: '/home',
     name: 'home',
     component: HomeView,
@@ -19,14 +23,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'login',
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
-  // {
-  //   path: '/home/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
 ]
 
 const router = createRouter({
